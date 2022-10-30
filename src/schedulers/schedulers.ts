26542f5fc946afb1ac72947c53b1ff386a1d7a69
Zerihun Moges <91532718+zerihunMoges@users.noreply.github.com>
2022-10-30 17:04:20 +0300

    Delete src directory

--- src/schedulers/schedulers.ts
+++
@@ -1,9 +0,0 @@
-export async function schedule(
-  func: Function,
-  time: number,
-  param = undefined
-) {
-  await new Promise((r) => setTimeout(r, time))
-  if (param) return await func(param)
-  else return await func()
-}
